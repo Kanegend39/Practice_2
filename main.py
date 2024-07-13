bdeede662a8777274c5b507897199ace8e1f8771
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2024-07-13 16:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
+# by Kanegend
 import math
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fft import rfft, rfftfreq
-from scipy.signal import hilbert
+import matplotlib.pyplot as plt
+from scipy.fft import fft2, fftfreq, ifft2
 
 
-def spectrum(A):    #функция рисует график спектра
-    S = rfft(A)
-    f = rfftfreq(5000, 1 / 1000) # 1 число - количество точек; 2 число - 1 / (количество точек в 1 секунде)
-    plt.plot(f, np.abs(S), color='red')
-    plt.xlabel("f, ГГц")
-    plt.ylabel('A.U.')
+def grafik_abs_U_X_Y(x, y, k, q):
+    X, Y = np.meshgrid(x, y)
+    U = abs((1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q)))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, U)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("U")
+    plt.show()
 
 
-t = np.arange(5, 10, 0.001)  # нс
-f_0 = 1  # ГГц
-w_0 = 2 * math.pi * f_0
-A_0 = 1
-A = A_0 * np.cos(w_0 * t)
-plt.xlabel("t, нс")
-plt.ylabel('A.U.')
-plt.grid(color='black', linewidth=0.5)
-plt.grid(True)
-plt.plot(t, A, color='orange')
-F = hilbert(A)
-F[0], F[-1] = 0, 0
-plt.plot(t, np.sqrt(F.real ** 2 + F.imag ** 2), color='green')
-#spectrum(A)
-plt.show()
+def grafik_kx_ky_F(x, y, k, q):
+    X, Y = np.meshgrid(x, y)
+    N = 2001
+    T = 20 / 2001
+    U = abs((1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q)))
+    F = fft2(U)
+    k = fftfreq(N, T)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(abs(k), abs(k), 1.0 / N * np.abs(F))
+    ax.set_xlabel("|kx|")
+    ax.set_ylabel("|ky|")
+    ax.set_zlabel("Fourie")
+    # ax.plot_surface(X, Y, np.abs(ifft2(F)))
+    # ax.set_xlabel("X")
+    # ax.set_ylabel("Y")
+    # ax.set_zlabel("U")
+    plt.show()
+
+
+def main():
+    f = 170 * 1e9  # в герцах
+    c = 3 * 1e10  # см/с
+    k = (2 * math.pi * f) / c
+    w = 0.5  # при z = 0 в см
+    lambda_ = (2 * math.pi) / k  # в см
+    x = np.arange(-10, 10.01, 0.01)
+    y = np.arange(-10, 10.01, 0.01)
+    q = - (math.pi * (w ** 2)) / (1j * lambda_)  # R(z=0) → ∞
+    # grafik_abs_U_X_Y(x, y, k, q)
+    grafik_kx_ky_F(x, y, k, q)
+
+
+main()
