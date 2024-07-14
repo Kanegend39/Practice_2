44d6c5f36cee193cf9cbd05b6cb12285cdc06674
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2024-07-14 21:06:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def grafik_abs_U_X_Y(x, y, k, q):
 def grafik_kx_ky_F(x, y, k, q):
     X, Y = np.meshgrid(x, y)
     N = 2001
-    T = 20 / 2001
+    T = 0.01  # timestep
     U = abs((1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q)))
     F = fft2(U)
     k = fftfreq(N, T)
+    print(max(k))
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(abs(k), abs(k), 1.0 / N * np.abs(F))
