479763a69cf4f88aee003aee846f276737fb43b2
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2024-09-14 22:59:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import math
 import cmath
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft2, fftfreq, ifft2
+from scipy.fft import fft2, fftfreq, ifftn, ifft2
 
 
 def grafik_abs_U_X_Y(x, y, k, q, z):
     X, Y = np.meshgrid(x, y)
     q += z
-    U = np.abs((1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q)))
+    U = (1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q))
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(X, Y, U)
+    ax.plot_surface(X, Y, abs(U))
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("U")
@@ -21,12 +21,12 @@ def grafik_abs_U_X_Y(x, y, k, q, z):
 def grafik_kx_ky_F(x, y, k, q, z):
     X, Y = np.meshgrid(x, y)
     N = 2048
-    T = 0.01  # timestep
-    U = np.abs((1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q)))
+    T = 0.01
+    U = (1 / q) * np.exp(-1j * k * ((X ** 2 + Y ** 2) / 2) * (1 / q))
     k_x_y = fftfreq(N, T)
     F = fft2(U)
     A, B = np.meshgrid(k_x_y, k_x_y)
-    F *= np.exp(1j * z * np.sqrt(k ** 2 - A ** 2 - B ** 2 + 0j))
+    # ifft2 doesnt work, need algorithm for fourier transform with sigma
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     # ax.plot_surface(abs(k_x_y), abs(k_x_y), 1.0 / N * np.abs(F))
@@ -47,7 +47,7 @@ def main():
     lambda_ = (2 * math.pi) / k  # в см
     x = np.arange(-10, 10.48, 0.01)
     y = np.arange(-10, 10.48, 0.01)
-    z = 100  # в см
+    z = 10  # в см
     q = 1j * ((math.pi * (w ** 2)) / lambda_)  # R(z=0) → ∞
     grafik_kx_ky_F(x, y, k, q, z)
     grafik_abs_U_X_Y(x, y, k, q, z)
--- task_2.py
+++ task_2.py
@@ -1,60 +1,55 @@
 import math
-import cmath
+import matplotlib.pyplot as plt
+import numpy as np
 
-f = 170 * (10 ** 9)  # GH
+f = 170 * 10e9  # GH
 epsilon_1 = 1
 epsilon_2 = 12
 epsilon_3 = 1
-n_1 = math.sqrt(epsilon_1)
-n_2 = math.sqrt(epsilon_2)
-n_3 = math.sqrt(epsilon_3)
-c = 3 * (10 ** 8)
-d_2 = 0.3  # meters
+n_1 = np.sqrt(epsilon_1)
+n_2 = np.sqrt(epsilon_2)
+n_3 = np.sqrt(epsilon_3)
+c = 3 * 10e8
+d_2 = 0.675 * 10e-3  # meters
 k_0 = (2 * math.pi * f) / c
-alpha = math.pi / 50  # radian (3.6 degrees)
-k_x = k_0 * math.sin(alpha)
-N_1 = math.sqrt(epsilon_1 - ((k_x ** 2) / (k_0 ** 2)))
-N_2 = math.sqrt(epsilon_2 - ((k_x ** 2) / (k_0 ** 2)))
-N_3 = math.sqrt(epsilon_3 - ((k_x ** 2) / (k_0 ** 2)))
-phi_2 = cmath.exp(1j * N_2 * k_0 * d_2)
-M_21_s = [(N_2 + N_1) / (2 * N_2), (N_1 - N_2) / (2 * N_2),  # transfer matrix, s - polarization
-          (N_1 - N_2) / (2 * N_2), (N_2 + N_1) / (2 * N_2)]
-M_21_p = [((epsilon_1 * N_2 + epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
-          # transfer matrix, p - polarization
-          ((epsilon_1 * N_2 - epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
-          ((epsilon_1 * N_2 - epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
-          ((epsilon_1 * N_2 + epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2]
-M_32_s = [(N_3 + N_2) / (2 * N_3), (N_2 - N_3) / (2 * N_3),  # transfer matrix, s - polarization
-          (N_2 - N_3) / (2 * N_3), (N_3 + N_2) / (2 * N_3)]
-M_32_p = [((epsilon_2 * N_3 + epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
-          # transfer matrix, p - polarization
-          ((epsilon_2 * N_3 - epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
-          ((epsilon_2 * N_3 - epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
-          ((epsilon_2 * N_3 + epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3]
+alpha = np.arange(0., 3.14 / 2, 0.01)
+k_x = k_0 * np.sin(alpha)
+N_1 = np.sqrt(epsilon_1 - ((k_x ** 2) / (k_0 ** 2)))
+N_2 = np.sqrt(epsilon_2 - ((k_x ** 2) / (k_0 ** 2)))
+N_3 = np.sqrt(epsilon_3 - ((k_x ** 2) / (k_0 ** 2)))
+phi_2 = np.exp(1j * N_2 * k_0 * d_2)
+M_21_s = np.array([(N_2 + N_1) / (2 * N_2), (N_1 - N_2) / (2 * N_2),  # transfer matrix, s - polarization
+                   (N_1 - N_2) / (2 * N_2), (N_2 + N_1) / (2 * N_2)])
+M_21_p = np.array([((epsilon_1 * N_2 + epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
+                   # transfer matrix, p - polarization
+                   ((epsilon_1 * N_2 - epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
+                   ((epsilon_1 * N_2 - epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2,
+                   ((epsilon_1 * N_2 + epsilon_2 * N_1) / (2 * N_2 * epsilon_1 * epsilon_2)) * n_1 * n_2])
+M_32_s = np.array([(N_3 + N_2) / (2 * N_3), (N_2 - N_3) / (2 * N_3),  # transfer matrix, s - polarization
+                   (N_2 - N_3) / (2 * N_3), (N_3 + N_2) / (2 * N_3)])
+M_32_p = np.array([((epsilon_2 * N_3 + epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
+                   # transfer matrix, p - polarization
+                   ((epsilon_2 * N_3 - epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
+                   ((epsilon_2 * N_3 - epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3,
+                   ((epsilon_2 * N_3 + epsilon_3 * N_2) / (2 * N_3 * epsilon_2 * epsilon_3)) * n_2 * n_3])
 PHI_2 = [phi_2, 0, 0, phi_2 ** (-1)]  # propagation matrix
 A_s = [M_32_s[0] * PHI_2[0] + M_32_s[1] * PHI_2[2], M_32_s[0] * PHI_2[2] + M_32_s[1] * PHI_2[3],
        M_32_s[2] * PHI_2[0] + M_32_s[3] * PHI_2[2], M_32_s[2] * PHI_2[2] + M_32_s[3] * PHI_2[3]]  # M32 * PHI2
-T_s = [A_s[0] * M_21_s[0] + A_s[1] * M_21_s[2], A_s[0] * M_21_s[2] + A_s[1] * M_21_s[3],
-       A_s[2] * M_21_s[0] + A_s[3] * M_21_s[2], A_s[2] * M_21_s[2] + A_s[3] * M_21_s[3]]
+Tw_s = [A_s[0] * M_21_s[0] + A_s[1] * M_21_s[2], A_s[0] * M_21_s[2] + A_s[1] * M_21_s[3],
+        A_s[2] * M_21_s[0] + A_s[3] * M_21_s[2], A_s[2] * M_21_s[2] + A_s[3] * M_21_s[3]]
 A_p = [M_32_p[0] * PHI_2[0] + M_32_p[1] * PHI_2[2], M_32_p[0] * PHI_2[2] + M_32_p[1] * PHI_2[3],
        M_32_p[2] * PHI_2[0] + M_32_p[3] * PHI_2[2], M_32_p[2] * PHI_2[2] + M_32_p[3] * PHI_2[3]]  # M32 * PHI2
-T_p = [A_p[0] * M_21_p[0] + A_p[1] * M_21_p[2], A_p[0] * M_21_p[2] + A_p[1] * M_21_p[3],
-       A_p[2] * M_21_p[0] + A_p[3] * M_21_p[2], A_p[2] * M_21_p[2] + A_p[3] * M_21_p[3]]
-R_s = - (T_s[2] / T_s[3])
-R_p = - (T_p[2] / T_p[3])
-T_p = cmath.sqrt(1 - (R_p ** 2))
-T_s = cmath.sqrt(1 - (R_s ** 2))
-print(R_p, R_s)
-print(T_p, T_s)
-print(R_p.real, T_p.real)
-print(R_s.real, T_s.real)
-a = (N_1 + N_2) * (N_2 - N_3) * cmath.exp(1j * N_2 * k_0 * d_2) + (N_1 - N_2) * (N_2 + N_3) * cmath.exp(
-    -1j * N_2 * k_0 * d_2)
-b = (N_1 - N_2) * (N_2 - N_3) * cmath.exp(1j * N_2 * k_0 * d_2) + (N_1 + N_2) * (N_2 + N_3) * cmath.exp(
-    -1j * N_2 * k_0 * d_2)
-print(-(a / b))
-c = (epsilon_2 * N_3 - epsilon_3 * N_2) * (epsilon_1 * N_2 + epsilon_2 * N_1) * cmath.exp(1j * N_2 * k_0 * d_2) + (
-        epsilon_2 * N_3 + epsilon_3 * N_2) * (epsilon_1 * N_2 - epsilon_2 * N_1) * cmath.exp(-1j * N_2 * k_0 * d_2)
-d = (epsilon_2 * N_3 - epsilon_3 * N_2) * (epsilon_1 * N_2 - epsilon_2 * N_1) * cmath.exp(1j * N_2 * k_0 * d_2) + (
-        epsilon_2 * N_3 + epsilon_3 * N_2) * (epsilon_1 * N_2 + epsilon_2 * N_1) * cmath.exp(-1j * N_2 * k_0 * d_2)
-print(-(c / d))
+Tw_p = [A_p[0] * M_21_p[0] + A_p[1] * M_21_p[2], A_p[0] * M_21_p[2] + A_p[1] * M_21_p[3],
+        A_p[2] * M_21_p[0] + A_p[3] * M_21_p[2], A_p[2] * M_21_p[2] + A_p[3] * M_21_p[3]]
+R_s = - (Tw_s[2] / Tw_s[3])
+R_p = - (Tw_p[2] / Tw_p[3])
+T_p = Tw_p[0] + R_p * Tw_p[1]
+T_s = Tw_s[0] + R_s * Tw_s[1]
+plt.plot(alpha, abs(R_p) ** 2, color='blue', label='p - polarization')
+plt.plot(alpha, abs(R_s) ** 2, color='green', label='s - polarization')
+# plt.plot(alpha, abs(T_p) ** 2, color='blue', label='p - polarization')
+# plt.plot(alpha, abs(T_s) ** 2, color='green', label='s - polarization')
+plt.legend(loc='best')
+plt.xlabel('alpha')
+plt.ylabel('|R|²')
+plt.show()
